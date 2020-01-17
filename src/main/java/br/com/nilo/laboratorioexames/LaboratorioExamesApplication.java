4acf51e7526c1ea61ea7410dec855dd3169b55a3
Nilo Fernandes Costa <ncosta.mazzatech@rd.com.br>
2020-01-17 20:41:28 -0300

    Criando estrutura do projeto em Maven e adicionando as dependencias

---
+++ src/main/java/br/com/nilo/laboratorioexames/LaboratorioExamesApplication.java
@@ -0,0 +1,13 @@
+package br.com.nilo.laboratorioexames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioExamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioExamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nilo/laboratorioexames/LaboratorioExamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nilo.laboratorioexames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioExamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
