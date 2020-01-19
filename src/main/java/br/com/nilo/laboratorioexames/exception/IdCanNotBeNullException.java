5002d10da7cbf9a3cba691463bed4abc54906c40
Nilo Fernandes Costa <ncosta.mazzatech@rd.com.br>
2020-01-19 16:12:24 -0300

    Criação de algumas exceptions para a camada de serviço

---
+++ src/main/java/br/com/nilo/laboratorioexames/exception/IdCanNotBeNullException.java
@@ -0,0 +1,30 @@
+package br.com.nilo.laboratorioexames.exception;
+
+public class IdCanNotBeNullException extends RuntimeException {
+
+	public IdCanNotBeNullException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public IdCanNotBeNullException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IdCanNotBeNullException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IdCanNotBeNullException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IdCanNotBeNullException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/br/com/nilo/laboratorioexames/exception/LaboratorioNotFoundException.java
@@ -0,0 +1,30 @@
+package br.com.nilo.laboratorioexames.exception;
+
+public class LaboratorioNotFoundException extends RuntimeException {
+
+	public LaboratorioNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LaboratorioNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LaboratorioNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LaboratorioNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LaboratorioNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
